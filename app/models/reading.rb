1e5590cd2ed39b32feeae1018981cfbe61b98d7b
Ankur Patel <ankur.patel@ymail.com>
2017-02-15 19:25:21 -0500

    Adding floor to search index

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -7,30 +7,20 @@ class Reading < ApplicationRecord
   delegate :location, to: :sensor
   attr_accessor :force_alert
 
-  settings index: { number_of_shards: 1 } do
-    mappings dynamic: 'false' do
-      indexes :temperature, type: :float
-      indexes :humidity, type: :float
-      indexes :updated_at, type: :date
-      indexes :sensor_id, analyzer: 'keyword'
-      indexes :room_name, analyzer: 'keyword'
-      indexes :location_name, analyzer: 'keyword'
-      indexes :organization_name, analyzer: 'keyword'
-    end
-  end
-
   after_create :should_create_notification?
 
   def as_indexed_json(options={})
     room = sensor.room
     location = room.location
+    floor = room.floor
     organization = location.organization
 
     self.as_json.merge(
       sensor_name: sensor.name,
       room_name: room.name,
       location_name: location.name,
-      organization_name: organization.name)
+      organization_name: organization.name,
+      floor: floor.name)
   end
 
   def should_create_notification?
