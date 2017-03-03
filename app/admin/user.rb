cc14d1354dfa550a4103ba19e74602e62deadc2b
Ankur Patel <ankur.patel@ymail.com>
2017-03-03 13:41:51 -0500

    Updating form to create new user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,7 +4,7 @@ config.filters = false
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :organization_id, :email, :name, :phone_number, :role_id
+permit_params :organization_id, :email, :name, :phone_number, :role_id, :password, :password_confirmation
 #
 # or
 #
@@ -27,6 +27,19 @@ permit_params :organization_id, :email, :name, :phone_number, :role_id
     column :phone_number
     column :role
     actions
+  end
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :organization
+      f.input :role
+      f.input :name
+      f.input :email
+      f.input :phone_number
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
   end
 
 end
