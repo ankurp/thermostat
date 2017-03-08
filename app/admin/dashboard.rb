83a931a3ea614c577fe64bb196eac952f2fef8d3
Ankur Patel <ankur.patel@ymail.com>
2017-03-07 19:55:33 -0500

    Setting width of 100% for chart

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
-      text_node %{<iframe src="http://kibana.encoredevlabs.com/app/kibana#/dashboard/Dashboard?embed=true&_g=(refreshInterval%3A('%24%24hashKey'%3A'object%3A1045'%2Cdisplay%3A'1%20minute'%2Cpause%3A!f%2Csection%3A2%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-24h%2Cmode%3Aquick%2Cto%3Anow))" height="600" width="800"></iframe>}.html_safe
+      text_node %{<iframe src="http://kibana.encoredevlabs.com/app/kibana#/dashboard/Dashboard?embed=true&_g=(refreshInterval%3A('%24%24hashKey'%3A'object%3A1045'%2Cdisplay%3A'1%20minute'%2Cpause%3A!f%2Csection%3A2%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-24h%2Cmode%3Aquick%2Cto%3Anow))" height="600" width="100%"></iframe>}.html_safe
     end
   end # content
 end
