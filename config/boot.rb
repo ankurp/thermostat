c71586ca7a0bd1997de9e56a12cd925dc645d709
Ankur Patel <ankur.patel@ymail.com>
2017-12-12 20:27:13 -0500

    Updating project

--- config/boot.rb
+++ config/boot.rb
@@ -37,14 +37,3 @@ module BootLib
     end
   end
 end
-
-BootLib::Require.from_gem('bootsnap', 'bootsnap')
-Bootsnap.setup(
-  cache_dir:            'tmp/cache', # Path to your cache
-  development_mode:     ENV['MY_ENV'] == 'development',
-  load_path_cache:      true,        # Should we optimize the LOAD_PATH with a cache?
-  autoload_paths_cache: true,        # Should we optimize ActiveSupport autoloads with cache?
-  disable_trace:        false,       # Sets `RubyVM::InstructionSequence.compile_option = { trace_instruction: false }`
-  compile_cache_iseq:   true,        # Should compile Ruby code into ISeq cache?
-  compile_cache_yaml:   true         # Should compile YAML into a cache?
-)
