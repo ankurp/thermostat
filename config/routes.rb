28bdc86d75243e7e41c456f1607e436c8cab3ee2
Ankur Patel <ankur.patel@ymail.com>
2017-12-12 21:11:06 -0500

    Updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  # root to: 'admin/dashboard#index'
-  root to: 'static#home'
+  root to: 'admin/dashboard#index'
 
   resources :readings, only: [:create]
 end
