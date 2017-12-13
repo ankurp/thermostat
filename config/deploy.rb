f0c46a2107ee39db66fceff22db1eea607e70677
Ankur Patel <ankur.patel@ymail.com>
2017-12-12 20:43:31 -0500

    Updating project

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "3.7.2"
 set :application, 'thermostat'
 set :repo_url, "git@github.com:ankurp/thermostat.git"
 
-set :user,            'ubuntu'
+set :user,            'admin'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -19,7 +19,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true }
+set :ssh_options,     { keys: ["#{ENV['HOME']}/certs/somsd.pem"], forward_agent: true }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+server '54.236.13.236', user: 'admin', roles: %w{app db web}, primary: true
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
