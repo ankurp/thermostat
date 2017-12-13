32810d5b732a716e76952d5968b69a2dfc40cea7
Ankur Patel <ankur.patel@ymail.com>
2017-12-13 09:37:40 -0500

    Updating resize logic

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
-      text_node %{<iframe src="http://thermostat.encoredevlabs.com/metrics/app/kibana#/dashboard/AWBQOv_VU69E-5U4orJU?embed=true&_g=(refreshInterval%3A('%24%24hashKey'%3A'object%3A261'%2Cdisplay%3A'1%20minute'%2Cpause%3A!f%2Csection%3A2%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-24h%2Cmode%3Aquick%2Cto%3Anow))" height="100%" width="100%"></iframe>}.html_safe
+      text_node %{<iframe scrolling="no" frameborder="0" src="http://thermostat.encoredevlabs.com/metrics/app/kibana#/dashboard/AWBQOv_VU69E-5U4orJU?embed=true&_g=(refreshInterval%3A('%24%24hashKey'%3A'object%3A261'%2Cdisplay%3A'1%20minute'%2Cpause%3A!f%2Csection%3A2%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-24h%2Cmode%3Aquick%2Cto%3Anow))" width="100%"></iframe>}.html_safe
     end
   end # content
 end
--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,13 @@
 //= require active_admin/base
+
+var iframe;
+function resizeFrame() {
+  if (!iframe) {
+    iframe = document.querySelector('iframe');
+  }
+  if (!iframe) {
+    return;
+  }
+  iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+}
+setInterval(resizeFrame, 1000);
